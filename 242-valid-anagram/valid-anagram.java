0bbd2635bf25590124e1b8c7a174276e0bf2b82b
Ananya Baghel <142593545+Annaa74@users.noreply.github.com>
2025-01-21 10:04:00 +0530

    Time: 4 ms (76.16%) | Memory: 44.9 MB (26.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,22 +1,5 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // slen = s.length();
-        // tlen = t.length();
-        // String dup = "";
-        // int countAL = 0;
-        // int counter = 0;
-        // if(slen.equals(tlen)){
-        //     for(int i = 0;i < slen ; i++){
-        //         char ch = s.charAt(i);
-        //         if(dup.contains(ch)){
-        //             counter +=1;
-        //         }
-        //         else{
-        //             dup = dup +ch;
-        //             countAL = 1;
-        //         }
-        //     }
-        // }
         if(s.length() != t.length()){
             return false;
         }
